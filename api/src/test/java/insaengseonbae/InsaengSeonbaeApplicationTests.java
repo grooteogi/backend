a57a91215cafe4ddc0566110dc8547eb65977e64
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-03-25 00:07:57 +0900

    feat: Create spring-boot project

---
+++ api/src/test/java/insaengseonbae/InsaengSeonbaeApplicationTests.java
@@ -0,0 +1,13 @@
+package insaengseonbae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsaengSeonbaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
