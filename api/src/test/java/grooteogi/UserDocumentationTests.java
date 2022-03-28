60beb1559f6f5f286d8dac1063199d5d5a96b83e
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-03-29 00:46:00 +0900

    feat: Add user documentation tests

---
+++ api/src/test/java/grooteogi/UserDocumentationTests.java
@@ -0,0 +1,72 @@
+package grooteogi;
+
+import static grooteogi.ApiDocumentUtils.getDocumentRequest;
+import static grooteogi.ApiDocumentUtils.getDocumentResponse;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import grooteogi.controller.UserController;
+import grooteogi.domain.User;
+import grooteogi.service.UserService;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
+@WebMvcTest(UserController.class)
+public class UserDocumentationTests {
+
+  @Autowired
+  protected MockMvc mockMvc;
+
+  @MockBean
+  private UserService userService;
+
+  @BeforeEach
+  void setUp(WebApplicationContext webApplicationContext,
+      RestDocumentationContextProvider restDocumentation) {
+    mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+        .apply(documentationConfiguration(restDocumentation)
+            .operationPreprocessors()
+            .withRequestDefaults(prettyPrint())
+            .withResponseDefaults(prettyPrint()))
+        .build();
+  }
+
+  @Test
+  void getMember() throws Exception {
+
+    List<User> userList = new ArrayList<>();
+    given(userService.getAllUser()).willReturn(userList);
+
+    ResultActions result = mockMvc.perform(
+        RestDocumentationRequestBuilders.get("/user")
+    );
+
+    result.andExpect(status().isOk())
+        .andDo(print())
+        .andDo(
+            document("user",
+                getDocumentRequest(),
+                getDocumentResponse()
+            )
+        );
+  }
+}
