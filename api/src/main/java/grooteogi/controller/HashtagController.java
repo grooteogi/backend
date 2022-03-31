0a20ba45fea8042838d020e4bf598a2ec142fb26
tnqls991213 <tnqls991213@icloud.com>
2022-03-31 22:56:05 +0900

    fix: Fix for add hashtag

--- api/src/main/java/grooteogi/controller/HashtagController.java
+++ api/src/main/java/grooteogi/controller/HashtagController.java
@@ -3,6 +3,8 @@ package grooteogi.controller;
 import grooteogi.domain.Hashtag;
 import grooteogi.service.HashtagService;
 import java.util.List;
+
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,23 +13,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class HashtagController {
   private final HashtagService hashtagService;
 
-  @Autowired
-  public HashtagController(HashtagService hashtagService) {
-    this.hashtagService = hashtagService;
-  }
 
 
-  @GetMapping({"/hashtag/getAll"})
+  @GetMapping("/hashtag")
   public ResponseEntity<List<Hashtag>> getAllHashtag() {
     return ResponseEntity.ok(this.hashtagService.getAllHashtag());
   }
 
-  @PostMapping({"/hashtag/create"})
+  @PostMapping("/hashtag/create")
   public ResponseEntity<Hashtag> createHashtag(@RequestParam String tag) {
-    Hashtag created = this.hashtagService.createHashtag(tag);
-    return ResponseEntity.ok(created);
+    Hashtag createdHashtag = this.hashtagService.createHashtag(tag);
+    return ResponseEntity.ok(createdHashtag);
   }
 }
