36da687b11c01772adde7d4e89c8bee14ac8307a
dldmsql <yhy5913@gmail.com>
2022-03-31 18:48:34 +0900

    feat: Login Oauth

--- api/src/main/java/grooteogi/dto/LoginDto.java
+++ api/src/main/java/grooteogi/dto/LoginDto.java
@@ -3,7 +3,6 @@ package grooteogi.dto;
 import lombok.Getter;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
 
 @Getter
 public class LoginDto {
@@ -11,6 +10,6 @@ public class LoginDto {
     @NotBlank(message = "이메일 주소를 입력해주세요.")
     private String email;
 
-    @NotBlank(message = "비밀번호를 입력해주세요.")
+//    @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
 }
