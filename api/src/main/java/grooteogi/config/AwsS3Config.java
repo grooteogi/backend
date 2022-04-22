859828dfa8fe90ae64decec83ad760428e09e9e9
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-04-22 20:41:13 +0900

    refactor: Add configure annotation in aws_s3_config

--- api/src/main/java/grooteogi/config/AwsS3Config.java
+++ api/src/main/java/grooteogi/config/AwsS3Config.java
@@ -7,7 +7,9 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AwsS3Config {
   @Value("${cloud.aws.credentials.access-key}")
   private String accessKey;
