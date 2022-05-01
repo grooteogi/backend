f35386449ae0d9dc6e533c7f22286d9bf22fa182
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-05-02 00:45:11 +0900

    refactor: Add email client

--- api/src/main/java/grooteogi/service/EmailService.java
+++ api/src/main/java/grooteogi/service/EmailService.java
@@ -4,14 +4,9 @@ import grooteogi.dto.auth.EmailCodeDto;
 import grooteogi.exception.ApiException;
 import grooteogi.exception.ApiExceptionEnum;
 import grooteogi.repository.UserRepository;
+import grooteogi.utils.EmailClient;
 import grooteogi.utils.RedisClient;
-import java.util.Random;
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,26 +14,10 @@ import org.springframework.stereotype.Service;
 public class EmailService {
 
   private final UserRepository userRepository;
-  private final JavaMailSender javaMailSender;
+  private final EmailClient emailClient;
   private final RedisClient redisClient;
-  private final String prefix = "email_verify";
 
-  @Value("${spring.mail.username}")
-  private String mailServer;
-
-  private static String createCode() {
-    Random random = new Random();
-    StringBuilder code = new StringBuilder();
-
-    for (int i = 0; i < 3; i++) {
-      int idx = random.nextInt(25) + 65;
-      code.append((char) idx);
-    }
-    int numIdx = random.nextInt(9999) + 1000;
-    code.append(numIdx);
-
-    return code.toString();
-  }
+  private final static String prefix = "email_verify";
 
   private boolean isExist(String email) {
     return userRepository.existsByEmail(email);
@@ -48,8 +27,11 @@ public class EmailService {
     if (isExist(email)) {
       throw new ApiException(ApiExceptionEnum.EMAIL_DUPLICATION_EXCEPTION);
     }
-    String code = createCode();
-    sendMail(email, code);
+    String code = emailClient.createCode();
+    emailClient.send(email, code);
+
+    String key = prefix + email;
+    redisClient.setValue(key, code, 3L);
   }
 
   public void checkVerifyEmail(EmailCodeDto emailCodeRequest) {
@@ -60,22 +42,4 @@ public class EmailService {
     }
   }
 
-  public void sendMail(String email, String code) {
-    String subject = "제목: 그루터기 회원 가입 인증 절차";
-    String text = "회원 가입을 위한 인증번호는 " + code + " 입니다. " + "제한 시간 3분 이내에 인증번호를 입력해주세요.";
-
-    try {
-      MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-      MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "utf-8");
-      helper.setFrom(mailServer);
-      helper.setTo(email);
-      helper.setSubject(subject);
-      helper.setText(text, true);
-      javaMailSender.send(mimeMessage);
-    } catch (MessagingException e) {
-      e.printStackTrace();
-    }
-    String key = prefix + email;
-    redisClient.setValue(key, code, 3L);
-  }
 }
