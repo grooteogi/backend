537c61fc1de0b30dd8eec664e661afd811fcc544
kyuriiii <47288015+kyuriiii@users.noreply.github.com>
2022-05-04 15:59:27 +0900

    refactor: Oauth code refactoring

--- api/src/main/java/grooteogi/controller/AuthController.java
+++ api/src/main/java/grooteogi/controller/AuthController.java
@@ -1,15 +1,18 @@
 package grooteogi.controller;
 
 import grooteogi.domain.User;
+import grooteogi.dto.OauthDto;
 import grooteogi.dto.auth.EmailCodeDto;
 import grooteogi.dto.auth.LoginDto;
 import grooteogi.dto.auth.Token;
 import grooteogi.dto.auth.UserDto;
+import grooteogi.enums.LoginType;
 import grooteogi.exception.ApiException;
 import grooteogi.exception.ApiExceptionEnum;
 import grooteogi.response.BasicResponse;
 import grooteogi.service.AuthService;
 import grooteogi.service.UserService;
+import grooteogi.utils.OauthClient;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
@@ -23,6 +26,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -38,17 +42,14 @@ public class AuthController {
 
   private final UserService userService;
   private final AuthService authService;
+  private final OauthClient oauthClient;
 
   @PostMapping("/login")
   public ResponseEntity<BasicResponse> login(@RequestBody LoginDto loginDto) {
     User user = userService.getUserByEmail(loginDto.getEmail());
     Token token = authService.login(user, loginDto);
 
-    HttpHeaders responseHeaders = new HttpHeaders();
-    responseHeaders.set("X-AUTH-TOKEN", token.getAccessToken());
-    ResponseCookie responseCookie = ResponseCookie.from("X-REFRESH-TOKEN", token.getRefreshToken())
-        .httpOnly(true).secure(true).path("/").build();
-    responseHeaders.set(HttpHeaders.SET_COOKIE, responseCookie.toString());
+    HttpHeaders responseHeaders = setHeader(token, true);
 
     return new ResponseEntity<>(BasicResponse.builder().data(user).build(), responseHeaders,
         HttpStatus.OK);
@@ -98,8 +99,10 @@ public class AuthController {
       throw new ApiException((ApiExceptionEnum) result.get("status"));
     }
 
-    HttpHeaders responseHeaders = new HttpHeaders();
-    responseHeaders.set("X-AUTH-TOKEN", result.get("token").toString());
+    Token token = new Token();
+    token.setAccessToken(result.get("token").toString());
+
+    HttpHeaders responseHeaders = setHeader(token, false);
     User user = userService.getUser(Integer.parseInt(result.get("ID").toString()));
 
     return new ResponseEntity<>(BasicResponse.builder().data(user).build(), responseHeaders,
@@ -128,5 +131,44 @@ public class AuthController {
 
     return ResponseEntity.ok(
         BasicResponse.builder().message("confirm email verification success").build());
+  }
+
+  @GetMapping("/oauth/{type}")
+  public ResponseEntity<BasicResponse> oauthGoogle(
+      @PathVariable String type, @RequestParam("code") String code) {
+    OauthDto oauthDto = new OauthDto();
+    oauthDto.setCode(code);
+    UserDto userDto;
+
+    if (type.toUpperCase().equals(LoginType.GOOGLE)) {
+      oauthDto.setType(LoginType.GOOGLE);
+      userDto = oauthClient.googleToken(oauthDto);
+    } else if (type.toUpperCase().equals(LoginType.KAKAO)) {
+      oauthDto.setType(LoginType.KAKAO);
+      userDto = oauthClient.kakaoToken(oauthDto);
+    } else {
+      throw new ApiException(ApiExceptionEnum.NOT_FOUND_EXCEPTION);
+    }
+
+    Map<String, Object> result = authService.oauth(userDto);
+
+    Token token = (Token) result.get("token");
+    HttpHeaders responseHeaders = setHeader(token, true);
+
+    return new ResponseEntity<>(BasicResponse.builder().data((User) result.get("user")).build(),
+        responseHeaders, HttpStatus.OK);
+  }
+
+  private HttpHeaders setHeader(Token token, boolean isRefresh) {
+    HttpHeaders responseHeaders = new HttpHeaders();
+    responseHeaders.set("X-AUTH-TOKEN", token.getAccessToken());
+
+    if (isRefresh) {
+      ResponseCookie responseCookie = ResponseCookie.from("X-REFRESH-TOKEN",
+          token.getRefreshToken()).httpOnly(true).secure(true).path("/").build();
+      responseHeaders.set(HttpHeaders.SET_COOKIE, responseCookie.toString());
+    }
+
+    return responseHeaders;
   }
 }
