bea7f2dda026f9fea3cf155bc71fd9cd22d71910
dldmsql <yhy5913@naver.com>
2022-05-05 13:26:27 +0900

    refactor: Edit exception about reservation

--- api/src/main/java/grooteogi/exception/ApiExceptionEnum.java
+++ api/src/main/java/grooteogi/exception/ApiExceptionEnum.java
@@ -33,6 +33,8 @@ public enum ApiExceptionEnum {
   POST_NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, "저장된 해당 포스트가 없습니다."),
   HASHTAG_NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, "저장된 해당 해시태그가 없습니다."),
   DUPLICATION_RESERVATION_EXCEPTION(HttpStatus.CONFLICT, "신청 불가능한 일정입니다."),
+  SCHEDULE_NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, "존재하지 않는 일정입니다."),
+  RESERVATION_NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, "예약이 존재하지 않습니다."),
   INVALID_FILE_FORMAT_EXCEPTION(HttpStatus.BAD_REQUEST, "잘못된 파일 형식입니다.");
 
   private final HttpStatus httpStatus;
--- api/src/main/java/grooteogi/service/ReservationService.java
+++ api/src/main/java/grooteogi/service/ReservationService.java
@@ -1,12 +1,10 @@
 package grooteogi.service;
 
 import grooteogi.domain.Reservation;
-import grooteogi.domain.Schedule;
 import grooteogi.dto.ReservationDto;
 import grooteogi.exception.ApiException;
 import grooteogi.exception.ApiExceptionEnum;
 import grooteogi.repository.ReservationRepository;
-import grooteogi.repository.ScheduleRepository;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -19,12 +17,11 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ReservationService {
   private final ReservationRepository reservationRepository;
-  private final ScheduleRepository scheduleRepository;
 
   public List<Reservation> getAllReservation() {
     List<Reservation> reservations = reservationRepository.findAll();
     if(reservations.isEmpty()) {
-      throw new ApiException(ApiExceptionEnum.NOT_FOUND_EXCEPTION);
+      throw new ApiException(ApiExceptionEnum.RESERVATION_NOT_FOUND_EXCEPTION);
     }
     return reservations;
   }
@@ -32,20 +29,21 @@ public class ReservationService {
   public Reservation getReservation(Integer reservationId) {
     Optional<Reservation> reservation = reservationRepository.findById(reservationId);
     if (reservation.isEmpty()) {
-      throw new ApiException(ApiExceptionEnum.NOT_FOUND_EXCEPTION);
+      throw new ApiException(ApiExceptionEnum.RESERVATION_NOT_FOUND_EXCEPTION);
     }
     return reservation.get();
   }
 
   public Reservation createReservation(ReservationDto reservationDto) {
-    Optional<Schedule> schedule = scheduleRepository.findById(reservationDto.getScheduleId());
-    if (schedule.isEmpty()) {
-      throw new ApiException(ApiExceptionEnum.NOT_FOUND_EXCEPTION);
-    }
-    Optional<Reservation> reservation = reservationRepository.findByScheduleId(schedule.get().getId());
+    Optional<Reservation> reservation = reservationRepository.findByScheduleId(reservationDto.getScheduleId());
     if(reservation.isPresent()) {
       throw new ApiException(ApiExceptionEnum.DUPLICATION_RESERVATION_EXCEPTION);
     }
+
+    if(reservation.get().getSchedule() == null) {
+      throw new ApiException(ApiExceptionEnum.SCHEDULE_NOT_FOUND_EXCEPTION);
+    }
+
     Reservation createdReservation = new Reservation();
     BeanUtils.copyProperties(reservationDto, createdReservation);
     createdReservation.setCreateDate(Timestamp.valueOf(LocalDateTime.now()));
