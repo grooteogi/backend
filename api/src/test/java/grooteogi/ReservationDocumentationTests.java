963039e395520d848c278729f35e381c334d1704
dldmsql <yhy5913@naver.com>
2022-05-10 13:56:53 +0900

    test: Add documentation code about reservation

--- api/src/test/java/grooteogi/ReservationDocumentationTests.java
+++ api/src/test/java/grooteogi/ReservationDocumentationTests.java
@@ -1,30 +1,39 @@
 package grooteogi;
 
+import static grooteogi.ApiDocumentUtils.getDocumentRequest;
+import static grooteogi.ApiDocumentUtils.getDocumentResponse;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import grooteogi.config.UserInterceptor;
 import grooteogi.controller.ReservationController;
+import grooteogi.domain.Hashtag;
 import grooteogi.domain.Post;
+import grooteogi.domain.PostHashtag;
 import grooteogi.domain.Reservation;
 import grooteogi.domain.Schedule;
 import grooteogi.domain.User;
 import grooteogi.dto.ReservationDto;
 import grooteogi.enums.CreditType;
 import grooteogi.enums.LoginType;
+import grooteogi.mapper.ReservationMapper;
 import grooteogi.service.ReservationService;
 import grooteogi.utils.JwtProvider;
 import grooteogi.utils.Session;
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.sql.Date;
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
@@ -38,6 +47,7 @@ import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -72,12 +82,13 @@ public class ReservationDocumentationTests {
   private SecurityContext securityContext;
 
   User hostUser;
-  User particiUser;
+  User participateUser;
   Post post;
   Schedule schedule;
   List<Schedule> schedules;
   ReservationDto.Request request;
-  Reservation response;
+  Reservation reservation;
+  List<PostHashtag> tags;
 
   @BeforeEach
   void setUp(WebApplicationContext webApplicationContext,
@@ -87,60 +98,86 @@ public class ReservationDocumentationTests {
             .withRequestDefaults(prettyPrint()).withResponseDefaults(prettyPrint())).build();
 
     // domain for test
-    hostUser = new User();
-    hostUser.setId(1);
-    hostUser.setType(LoginType.GENERAL);
-    hostUser.setEmail("groot@example.com");
-    hostUser.setPassword(passwordEncoder.encode("groot1234*"));
-    hostUser.setNickname("groot-1");
+    hostUser = User.builder()
+        .email("groot@hello.com")
+        .nickname("host")
+        .password(passwordEncoder.encode("groot1234*"))
+        .type(LoginType.GENERAL)
+        .build();
 
-    particiUser = new User();
-    particiUser.setId(2);
-    particiUser.setType(LoginType.GENERAL);
-    particiUser.setEmail("groot22@example.com");
-    particiUser.setPassword(passwordEncoder.encode("groot1234*"));
-    particiUser.setNickname("groot-2");
+    participateUser = User.builder()
+        .email("groot2@hello.com")
+        .nickname("participant")
+        .password(passwordEncoder.encode("groot1234*"))
+        .type(LoginType.GENERAL)
+        .build();
 
     schedules = new ArrayList<>();
-    schedule = new Schedule();
-    schedule.setId(1);
-    schedule.setDate(LocalDate.now());
-    schedule.setRegion("인천");
-    schedule.setPlace("부평역");
-    schedule.setStartTime(LocalTime.of(14, 00));
-    schedule.setEndTime(LocalTime.of(15, 00));
+    schedule = Schedule.builder()
+        .date(Date.valueOf("2022-05-07"))
+        .startTime(Time.valueOf("11:00:00"))
+        .endTime(Time.valueOf("12:00:00"))
+        .region("서대문구")
+        .place("명지대")
+        .build();
     schedules.add(schedule);
 
-    post = new Post();
-    post.setId(1);
-    post.setUser(hostUser);
-    post.setPostHashtags(null);
+    tags = new ArrayList<>();
+    PostHashtag tag = PostHashtag.builder()
+        .hashTag(Hashtag.builder().tag("해시태그").build())
+        .build();
+    tags.add(tag);
+
+    post = Post.builder()
+        .content("내용이다.")
+        .title("제목이다")
+        .credit(CreditType.DIRECT)
+        .imageUrl("이미지 주소다")
+        .postHashtags(tags)
+        .build();
 
     schedule.setPost(post);
 
-    post.setSchedules(schedules);
-    post.setViews(0);
-    post.setTitle("제목이다.");
-    post.setContent("내용이다");
-    post.setCredit(CreditType.DIRECT);
-    post.setImageUrl("이미지 주소다");
+    reservation = Reservation.builder()
+        .schedule(schedule)
+        .hostUser(hostUser)
+        .participateUser(participateUser)
+        .message("msg")
+        .build();
   }
 
   @Test
   @DisplayName("예약조회")
   public void getReservation() throws Exception {
     // given
-    Reservation reservation = reservationEntity();
-    given(reservationService.getReservation(1)).willReturn(reservation);
-    String reservationId = "1";
+    given(reservationService.getReservation(1)).willReturn(reservationRes());
 
     ResultActions result = mockMvc.perform(
         RestDocumentationRequestBuilders
-            .get("/reservation/{reservationId}", reservationId)
+            .get("/reservation/{reservationId}", 1)
             .characterEncoding("utf-8")
             .accept(MediaType.APPLICATION_JSON));
     result.andExpect(status().isOk())
-        .andDo(print());
+        .andDo(print()).andDo(
+            document("get-reservation", getDocumentRequest(), getDocumentResponse(),
+                responseFields(fieldWithPath("status").description("결과 코드"),
+                    fieldWithPath("data.reservationId").description("아이디"),
+                    fieldWithPath("data.imageUrl").type(JsonFieldType.STRING)
+                        .description("이미지 주소"),
+                    fieldWithPath("data.title").type(JsonFieldType.STRING)
+                        .description("포스트 제목"),
+                    fieldWithPath("data.hashtags").type(JsonFieldType.ARRAY)
+                        .description("포스트 해시태그"),
+                    fieldWithPath("data.date").type(JsonFieldType.STRING)
+                        .description("약속 날짜"),
+                    fieldWithPath("data.startTime").type(JsonFieldType.STRING)
+                        .description("약속 시작 시간"),
+                    fieldWithPath("data.endTime").type(JsonFieldType.STRING)
+                        .description("약속 끝 시간"),
+                    fieldWithPath("data.place").type(JsonFieldType.STRING)
+                        .description("약속 장소")))
+        );
+
     verify(reservationService).getReservation(1);
   }
 
@@ -166,8 +203,25 @@ public class ReservationDocumentationTests {
     );
 
     // then
-    resultActions.andExpect(status().isOk()).andDo(print());
-
+    resultActions.andExpect(status().isOk()).andDo(print()).andDo(
+        document("get-host-reservation", getDocumentRequest(), getDocumentResponse(),
+            responseFields(fieldWithPath("status").description("결과 코드"),
+                fieldWithPath("data.[].reservationId").description("아이디"),
+                fieldWithPath("data.[].imageUrl").type(JsonFieldType.STRING)
+                    .description("이미지 주소"),
+                fieldWithPath("data.[].title").type(JsonFieldType.STRING)
+                    .description("포스트 제목"),
+                fieldWithPath("data.[].hashtags").type(JsonFieldType.ARRAY)
+                    .description("포스트 해시태그"),
+                fieldWithPath("data.[].date").type(JsonFieldType.STRING)
+                    .description("약속 날짜"),
+                fieldWithPath("data.[].startTime").type(JsonFieldType.STRING)
+                    .description("약속 시작 시간"),
+                fieldWithPath("data.[].endTime").type(JsonFieldType.STRING)
+                    .description("약속 끝 시간"),
+                fieldWithPath("data.[].place").type(JsonFieldType.STRING)
+                    .description("약속 장소")))
+    );
     verify(reservationService).getHostReservation(anyInt());
 
   }
@@ -194,7 +248,25 @@ public class ReservationDocumentationTests {
     );
 
     // then
-    resultActions.andExpect(status().isOk()).andDo(print());
+    resultActions.andExpect(status().isOk()).andDo(print()).andDo(
+        document("get-participate-reservation", getDocumentRequest(), getDocumentResponse(),
+            responseFields(fieldWithPath("status").description("결과 코드"),
+                fieldWithPath("data.[].reservationId").description("아이디"),
+                fieldWithPath("data.[].imageUrl").type(JsonFieldType.STRING)
+                    .description("이미지 주소"),
+                fieldWithPath("data.[].title").type(JsonFieldType.STRING)
+                    .description("포스트 제목"),
+                fieldWithPath("data.[].hashtags").type(JsonFieldType.ARRAY)
+                    .description("포스트 해시태그"),
+                fieldWithPath("data.[].date").type(JsonFieldType.STRING)
+                    .description("약속 날짜"),
+                fieldWithPath("data.[].startTime").type(JsonFieldType.STRING)
+                    .description("약속 시작 시간"),
+                fieldWithPath("data.[].endTime").type(JsonFieldType.STRING)
+                    .description("약속 끝 시간"),
+                fieldWithPath("data.[].place").type(JsonFieldType.STRING)
+                    .description("약속 장소")))
+    );
 
     verify(reservationService).getUserReservation(anyInt());
   }
@@ -204,13 +276,12 @@ public class ReservationDocumentationTests {
   public void createReservation() throws Exception {
     // given
     request = reservationReq();
-    response = reservationEntity();
 
     when(securityContext.getAuthentication()).thenReturn(authentication);
     SecurityContextHolder.setContext(securityContext);
     when(SecurityContextHolder.getContext().getAuthentication().getPrincipal()).thenReturn(session);
 
-    given(reservationService.createReservation(eq(request), anyInt())).willReturn(response);
+    given(reservationService.createReservation(eq(request), anyInt())).willReturn(reservationRes());
 
     String json = objectMapper.writeValueAsString(request);
 
@@ -224,7 +295,27 @@ public class ReservationDocumentationTests {
 
     // then
     resultActions.andExpect(status().isOk())
-        .andDo(print());
+        .andDo(print()).andDo(
+            document("create-reservation", getDocumentRequest(), getDocumentResponse(),
+                requestFields(fieldWithPath("scheduleId").description("스케쥴 아이디"),
+                    fieldWithPath("message").description("메세지")),
+                responseFields(fieldWithPath("status").description("결과 코드"),
+                    fieldWithPath("data.reservationId").description("아이디"),
+                    fieldWithPath("data.imageUrl").type(JsonFieldType.STRING)
+                        .description("이미지 주소"),
+                    fieldWithPath("data.title").type(JsonFieldType.STRING)
+                        .description("포스트 제목"),
+                    fieldWithPath("data.hashtags").type(JsonFieldType.ARRAY)
+                        .description("포스트 해시태그"),
+                    fieldWithPath("data.date").type(JsonFieldType.STRING)
+                        .description("약속 날짜"),
+                    fieldWithPath("data.startTime").type(JsonFieldType.STRING)
+                        .description("약속 시작 시간"),
+                    fieldWithPath("data.endTime").type(JsonFieldType.STRING)
+                        .description("약속 끝 시간"),
+                    fieldWithPath("data.place").type(JsonFieldType.STRING)
+                        .description("약속 장소")))
+        );
 
   }
 
@@ -244,20 +335,12 @@ public class ReservationDocumentationTests {
 
     //then
     resultActions.andExpect(status().isOk())
-        .andDo(print());
-
+        .andDo(print()).andDo(
+            document("delete-reservation", getDocumentRequest(), getDocumentResponse(),
+                responseFields(fieldWithPath("status").description("결과 코드"),
+                    fieldWithPath("message").description("응답 메세지")))
+        );
     verify(reservationService).deleteReservation(reservationId);
-  }
-
-  private Reservation reservationEntity() {
-    Reservation reservation = new Reservation();
-    reservation.setId(1);
-    reservation.setMessage("msg");
-    reservation.setHostUser(hostUser);
-    reservation.setSchedule(schedule);
-    reservation.setParticipateUser(particiUser);
-
-    return reservation;
   }
 
   private ReservationDto.Request reservationReq() {
@@ -269,16 +352,11 @@ public class ReservationDocumentationTests {
   }
 
   private ReservationDto.Response reservationRes() {
-    return ReservationDto.Response
-        .builder()
-        .reservationId(1)
-        .date(schedule.getDate())
-        .endTime(schedule.getEndTime())
-        .place(schedule.getPlace())
-        .startTime(schedule.getStartTime())
-        .imgUrl(post.getImageUrl())
-        .title(post.getTitle())
-        .hashtags(null)
-        .build();
+    List<String> stringTags = new ArrayList<>();
+    tags.forEach(postHashtag -> stringTags.add(postHashtag.getHashTag().getTag()));
+    ReservationDto.Response response = ReservationMapper
+        .INSTANCE.toResponseDto(reservation, post, schedule);
+    response.setHashtags(stringTags);
+    return response;
   }
 }
