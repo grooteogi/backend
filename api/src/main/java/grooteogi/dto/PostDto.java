6f858b6c83191542eadbf0c6e57f04a59b23a7f4
dldmsql <yhy5913@naver.com>
2022-05-10 18:51:23 +0900

    refactor: Create schedule dto

--- api/src/main/java/grooteogi/dto/PostDto.java
+++ api/src/main/java/grooteogi/dto/PostDto.java
@@ -1,6 +1,7 @@
 package grooteogi.dto;
 
 import grooteogi.enums.CreditType;
+import java.util.List;
 import javax.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Data;
@@ -27,7 +28,26 @@ public class PostDto {
 
     private String[] hashtags;
 
-    private Object[] schedules;
+    private List<ScheduleDto.Request> schedules;
   }
 
+  @Data
+  @Builder
+  public static class Response {
+
+    private String title;
+
+    private String content;
+
+    private String imageUrl;
+
+    private int views;
+
+    private CreditType credit;
+
+    private String nickname;
+
+    private String date;
+
+  }
 }
---
+++ api/src/main/java/grooteogi/dto/ScheduleDto.java
@@ -0,0 +1,23 @@
+package grooteogi.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+public class ScheduleDto {
+
+  @Data
+  @Builder
+  public static class Request {
+
+    private String date;
+
+    private String startTime;
+
+    private String endTime;
+
+    private String region;
+
+    private String place;
+
+  }
+}
