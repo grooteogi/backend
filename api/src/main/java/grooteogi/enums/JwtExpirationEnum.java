6c7cf825aa3f5ed883176fdbe0b57607727cb019
Kyuri <47288015+kyuriiii@users.noreply.github.com>
2022-05-16 15:01:35 +0900

    feat: Back jwt token time

--- api/src/main/java/grooteogi/enums/JwtExpirationEnum.java
+++ api/src/main/java/grooteogi/enums/JwtExpirationEnum.java
@@ -7,8 +7,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum JwtExpirationEnum {
 
-  ACCESS_TOKEN_EXPIRATION_TIME("JWT 만료 시간 / 30분", 1000L * 60 * 10), // * 30 추가 필요 현재 테스트를 위해 1분으로 설정
-  REDIS_TOKEN_EXPIRATION_TIME("REDIS 만료 시간 / 30분", 30L), // * 30 추가 필요 현재 테스트를 위해 1분으로 설정
+  ACCESS_TOKEN_EXPIRATION_TIME("JWT 만료 시간 / 30분", 1000L * 60), // * 30 추가 필요 현재 테스트를 위해 1분으로 설정
+  REDIS_TOKEN_EXPIRATION_TIME("REDIS 만료 시간 / 30분", 1L), // * 30 추가 필요 현재 테스트를 위해 1분으로 설정
   REFRESH_TOKEN_EXPIRATION_TIME("Refresh 토큰 만료 시간 / 7일", 1000L * 60 * 60 * 24 * 7);
 
   private String description;
