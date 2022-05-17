3098c434d6e651c13c01b0a8961b6e2c216ea4aa
dldmsql <yhy5913@naver.com>
2022-05-18 01:32:46 +0900

    fix: Add and change method in repo for detail response

--- api/src/main/java/grooteogi/repository/ReviewRepository.java
+++ api/src/main/java/grooteogi/repository/ReviewRepository.java
@@ -1,2 +1,15 @@
-package grooteogi.repository;public interface ReviewRepository {
+package grooteogi.repository;
+
+import grooteogi.domain.Review;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface ReviewRepository extends JpaRepository<Review, Integer> {
+
+  @Query(
+      value = "select * from review where schedule.post_id = :id",
+      nativeQuery = true
+  )
+  List<Review> findByPostId(Integer postId);
 }
