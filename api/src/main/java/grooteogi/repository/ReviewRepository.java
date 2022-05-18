61f390e904b75d8de1a2183e90f50f7e44bf5ca9
dldmsql <yhy5913@naver.com>
2022-05-18 10:16:05 +0900

    fix: Add find reservation query in reservation repo

--- api/src/main/java/grooteogi/repository/ReviewRepository.java
+++ api/src/main/java/grooteogi/repository/ReviewRepository.java
@@ -4,12 +4,13 @@ import grooteogi.domain.Review;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
 
   @Query(
-      value = "select * from review where schedule.post_id = :id",
+      value = "select * from review where review.post_id = :id",
       nativeQuery = true
   )
-  List<Review> findByPostId(Integer postId);
+  List<Review> findByPostId(@Param("id")Integer postId);
 }
