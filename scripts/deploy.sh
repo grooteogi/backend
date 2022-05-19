828544648e6ba8e4a81aa590d9ba4c11005309df
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-05-19 16:45:40 +0900

    chore: Add deploy.sh to deploy

---
+++ scripts/deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "> copy build file"
+REPOSITORY=/home/ubuntu/app/grooteogi
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
+
+echo "> check running application pid"
+CURRENT_PID=$(pgrep -f Grooteogi | grep jar | awk '{print$1}')
+
+echo "$CURRENT_PID"
+if [ -z $CURRENT_PID ]; then
+  echo "> running application not found!"
+else
+  kill -9 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> Deploy new application"
+JAR_NAME=$(ls -tr $REPOSITORY/*jar | tail -n 1)
+
+echo "> Grant execute permission for $JAR_NAME"
+chmod +x $JAR_NAME
+
+echo "> Run $JAR_NAME"
+nohup java -jar \
+  -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties,classpath:/application-real.properties \
+  -Dspring.profiles.active=real \
+  $JAR_NAME >$REPOSITORY/nohup.out 2>&1 &
