de52d8999eb680c888e0d5b0cb9b5160ae3a4768
dldmsql <yhy5913@naver.com>
2022-05-19 17:17:19 +0900

    fix: Add modify fun in mapper

--- api/src/main/java/grooteogi/mapper/ReviewMapper.java
+++ api/src/main/java/grooteogi/mapper/ReviewMapper.java
@@ -1,16 +1,33 @@
 package grooteogi.mapper;
 
+import grooteogi.domain.Post;
+import grooteogi.domain.Reservation;
 import grooteogi.domain.Review;
+import grooteogi.domain.User;
 import grooteogi.dto.ReviewDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(
+@Mapper(componentModel = "spring",
     unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ReviewMapper extends BasicMapper<ReviewDto, Review> {
 
   ReviewMapper INSTANCE = Mappers.getMapper(ReviewMapper.class);
 
-  Review toEntity(ReviewDto.Request dto);
+  @Mapping(source = "dto.text", target = "text")
+  @Mapping(source = "dto.score", target = "score")
+  @Mapping(source = "post", target = "post")
+  @Mapping(source = "reservation", target = "reservation")
+  @Mapping(source = "user", target = "user")
+  @Mapping(target = "id", ignore = true)
+  @Mapping(target = "createAt", ignore = true)
+  @Mapping(target = "updateAt", ignore = true)
+  Review toEntity(ReviewDto.Request dto, Post post, Reservation reservation, User user);
+
+  @Mapping(source = "dto.text", target = "text")
+  @Mapping(source = "dto.score", target = "score")
+  @Mapping(source = "review.id", target = "id")
+  Review toModify(ReviewDto.Request dto, Review review);
 }
