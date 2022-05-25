eba4530d5d7d3b87bf9301f5bd9423ade0da7054
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-05-25 21:14:21 +0900

    refactor: Separate webconfig according to profile

--- api/src/main/java/grooteogi/config/WebConfig.java
+++ api/src/main/java/grooteogi/config/WebConfig.java
@@ -5,31 +5,47 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-@RequiredArgsConstructor
+
 public class WebConfig implements WebMvcConfigurer {
 
-  @Value("${spring.origin.url}")
-  private String url;
+  @Configuration
+  @Profile("local")
+  public class LocalConfig implements WebMvcConfigurer {
 
-  @Value("${spring.interceptor.excludes}")
-  private List<String> excludes;
-
-  private final JwtProvider jwtProvider;
-
-  @Override
-  public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**").allowedOrigins(url).allowedMethods("*");
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+      registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+    }
   }
 
-  @Override
-  public void addInterceptors(InterceptorRegistry registry) {
-    registry.addInterceptor(new UserInterceptor(jwtProvider))
-        .addPathPatterns("/**")
-        .excludePathPatterns(excludes);
+  @Configuration
+  @Profile("!local")
+  @RequiredArgsConstructor
+  public class ServerConfig implements WebMvcConfigurer {
+
+    @Value("${spring.origin.url}")
+    private String originUrl;
+
+    @Value("${spring.interceptor.excludes}")
+    private List<String> interceptorExcludes;
+
+    private final JwtProvider jwtProvider;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+      registry.addMapping("/**").allowedOrigins(originUrl).allowedMethods("*");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+      registry.addInterceptor(new UserInterceptor(jwtProvider))
+          .addPathPatterns("/**")
+          .excludePathPatterns(interceptorExcludes);
+    }
   }
 }
