357979163da0928d2e5c646d4e4aa2bdfd6772d8
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-05-29 09:25:37 +0900

    refactor: Remove unnecessary implements

--- api/src/main/java/grooteogi/config/WebConfig.java
+++ api/src/main/java/grooteogi/config/WebConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig {
 
   @Configuration
   @Profile("local")
